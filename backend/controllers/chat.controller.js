551f14ff03bf17ed7b8496fdff6d48e9c8123da0
sanchez-ramps <rodrigo.sanchez@rampslogistics.com>
2022-08-20 17:13:59 -0400

    load user chats

---
+++ backend/controllers/chat.controller.js
@@ -0,0 +1,52 @@
+
+const Chat = require("../models/chat.model");
+const User = require("../models/user.model");
+
+
+const getOtherUsers = (chats, id) => {
+    let results = []
+    for (let i = 0; i < chats.length; i++) {
+        let chat = chats[i];
+        let otherUserId = ''
+        chat.users.forEach(uid => {
+            if (uid != id) {
+                otherUserId = uid
+            }
+        })
+        chat.otherUserId = otherUserId
+        console.log(chat.otherUserId, otherUserId)
+        results.push(chat)
+    }
+
+    return results
+}
+
+const add = (req, res) => {
+    let chat = new Chat(req.body);
+    chat.save()
+        .then(chat => {
+            res.status(200).json({ 'chat': 'Chat added successfully' });
+        })
+        .catch(err => {
+            res.status(400).send('Error adding a new Chat');
+        });
+};
+
+
+const getUserChats = (req, res) => {
+    Chat.find((err, chats) => {
+        if (err) {
+            console.log(err);
+        } else {
+            chats = chats.filter(chat => {
+                return chat.users.includes(req.params.id)
+            })
+
+            let updatedChats = getOtherUsers(chats, req.params.id)
+            res.json(updatedChats)
+
+        }
+    });
+};
+
+export { add, getUserChats };
