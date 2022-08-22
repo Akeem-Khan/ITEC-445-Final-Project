b5873a82ac36895d68dc6405aa51b34f21c172f6
sanchez-ramps <rodrigo.sanchez@rampslogistics.com>
2022-08-21 23:18:40 -0400

    create design update

--- ist-department/src/components/notice/create-notice.component.js
+++ ist-department/src/components/notice/create-notice.component.js
@@ -1,15 +1,32 @@
-import React, {useState, useContext} from 'react';
+import React, { useState, useContext } from 'react';
 import axios from 'axios';
 import AuthContext from "../../context/auth.context";
-
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Input from '@mui/material/Input';
 function CreateNotice() {
     const { user } = useContext(AuthContext);
-    
+
     const [title, setTitle] = useState("");
     const [text, setText] = useState("");
     const [category, setCategory] = useState("");
     const [author, setAuthor] = useState(user.email);
-
+    const theme = createTheme();
+    const categories = ["BSc Computer Information Systems", "BSc Information and Library Science", "BSc Information Technology", "BSc Internet Technology", "BSc Networking",
+    "AAS Information Systems Development", "AAS Internet Technology", "AAS Library and Information Studies", "AAS Operating Systems Management", "CISCO Certified Network Associate - CCNA",
+    "Certificate in Records Management", "Certificate in Records Management for the Public Sector"
+]
     async function submit(e) {
         e.preventDefault();
 
@@ -31,62 +48,64 @@ function CreateNotice() {
         setText("");
         setCategory("");
     }
-    
+
     return (
-        <div style={{marginTop: 20}}>
-            <h3>Create New Notice</h3>
-            <form onSubmit={submit}>
-                <div className="form-group">
-                    <label>Title: </label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                        required
-                    />
-                </div>
+        <ThemeProvider theme={theme}>
+            <Card className='mb-2'>
+                <CardContent>
+                    <Typography variant="h4" component="div">
+                        Create Notice
+                    </Typography>
 
-                <div className="form-group">
-                    <label>Text: </label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        value={text}
-                        onChange={(e) => setText(e.target.value)}
-                        required
-                    />
-                </div>
-                
-                <div className="form-group">
-                    <label>Category: </label>
-                    <br/>
-                    <select value={category} onChange={(e) => setCategory(e.target.value)}>
-                        <option value="----------">----------</option>
+                    <form>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="title"
+                            label="Title"
+                            name="title"
+                            autoFocus
+                            onChange={(e) => setTitle(e.target.value)}
+                            value={title}
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="text"
+                            label="Text"
+                            name="text"
+                            autoFocus
+                            onChange={(e) => setText(e.target.value)}
+                            value={text}
+                        />
 
-                        <option value="BSc Computer Information Systems">BSc Computer Information Systems</option>
-                        <option value="BSc Information and Library Science">BSc Information and Library Science</option>
-                        <option value="BSc Information Technology">BSc Information Technology</option>
-                        <option value="BSc Internet Technology">BSc Internet Technology</option>
-                        <option value="BSc Networking">BSc Networking</option>
+                        <FormControl fullWidth>
+                            <InputLabel>Category</InputLabel>
+                            <Select
+                                id="category"
+                                value={category}
+                                label="Category"
+                                onChange={(e) => setCategory(e.target.value)}
+                            >
 
-                        <option value="AAS Information Systems Development">AAS Information Systems Development</option>
-                        <option value="AAS Information Technology">AAS Information Technology</option>
-                        <option value="AAS Internet Technology">AAS Internet Technology</option>
-                        <option value="AAS Library and Information Studies">AAS Library and Information Studies</option>
-                        <option value="AAS Operating Systems Management">AAS Operating Systems Management</option>
-                        
-                        <option value="CISCO Certified Network Associate - CCNA">CISCO Certified Network Associate - CCNA</option>
-                        <option value="Certificate in Records Management">Certificate in Records Management</option>
-                        <option value="Certificate in Records Management for the Public Sector">Certificate in Records Management for the Public Sector</option>
-                    </select>
-                </div>
-                <br/>
-                <div className="form-group">
-                    <input type="submit" value="Submit" className="btn btn-primary" />
-                </div>
-            </form>
-        </div>
+                                {categories.map(category => {
+                                    return (
+                                        <MenuItem value={category} key={category}>{category}</MenuItem>)
+                                })}
+                            </Select>
+                        </FormControl>
+
+                    </form>
+
+
+                    <CardActions>
+                        <Button onClick={submit}>Submit</Button>
+                    </CardActions>
+                </CardContent>
+            </Card>
+        </ThemeProvider>
     )
 }
 
